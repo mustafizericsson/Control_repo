179a6cf0eebfd94ac4bf9befe691b000de9a16af
mustafizericsson <52062464+mustafizericsson@users.noreply.github.com>
2020-04-21 14:02:01 +0000

    Update ssh_server.pp

--- manifests/ssh_server.pp
+++ manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCUxgi+6RehGkzHcrm1rXsD4dMe5KRuLCtDMtcBE32ls3j3dbuZocvr4NEXUIaHC3kxho9pkOxH/9fMxN/q/d3Yrw7GhQT70XU1KzCe1Ne4sjTcvy3iOaWzRGmu4plQbb3y4AViCtElL0E+SljOOwFDTr0b3+Mo/UF4pNEDBjW8DdI71laLfAcnQNc64FQTT1r3lRF351BGm9hsB7xqCw2FhwVKiq1v3L+UfPfpXmAj/PxbiMa9390l5NscOEHik/BDV+8y3McgR46ib2qss+PI3X82O4PYzLw9xISTy7G3lsPjCP+YGQnkbW2M8h0eQoR08boGfnHNqBJwvEHJXD2H root@master.puppet.vm',
 	}  
 }
